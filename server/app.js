fbca46d98296225436cf0c8eea6338ad4b09b344
CodeWithRajMishra <rajmishra3@gmail.com>
2025-11-22 19:31:03 +0530

    multi

--- server/app.js
+++ server/app.js
@@ -4,6 +4,10 @@ const EmpRoute = require("./routes/empRoute");
 const cors= require("cors");
 const bodyparser = require('body-parser')
 const mongoose = require("mongoose");
+const multer = require("multer");
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require("./cloudinary");
+
 require("dotenv").config();
 // Body-parser middleware
 app.use(bodyparser.urlencoded({ extended: true }))
@@ -14,6 +18,42 @@ mongoose.connect(process.env.DBCONN).then(()=>{
 console.log(process.env.DBCONN);
 app.use(cors());
 app.use("/employees", EmpRoute);
+
+// Set up Cloudinary storage for multer
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'product_images', // folder name Cloudinary account
+        format: async (req, file) => 'jpg', // supports promises as well
+        public_id: (req, file) => Date.now() + '-' + file.originalname,
+    },
+});
+
+
+const upload = multer({ storage: storage }).array('images', 10); //image size
+
+
+
+app.post("/upload",  (req, res)=>{
+
+          upload(req, res, async (err) => {
+        if (err) {
+            return res.status(500).send("Error uploading files: " + err.message);
+        }
+
+
+         console.log(req.body);
+         console.log(req.files);
+         const imagePath = req.files.map(key=>key.path);
+         console.log(imagePath);
+
+
+
+}) ;
+  
+
+     res.send("File Uploaded!!!");
+})
 
 
 
