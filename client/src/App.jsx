ec2b5d713861f85faffc4d01732e128a0eaa8227
CodeWithRajMishra <rajmishra3@gmail.com>
2025-12-22 18:15:10 +0530

    totdodo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,59 @@
 import { useState } from "react";
-import { addTask, delTask } from "./todoSlice";
+import { addTask, delTask, taskComplete, taskIncomplete, taskUpdate } from "./todoSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { MdDeleteSweep } from "react-icons/md";
 import { FaCircleCheck } from "react-icons/fa6";
 import { ImCross } from "react-icons/im";
 const App=()=>{
   const [val, setVal]= useState("");
+  const [btnStatus, setBtnstatus]= useState(true);
+  const [taskId, setTaskId] = useState("");
   const dispatch = useDispatch();
   const myTask= useSelector(state=>state.todo.task);
   console.log(myTask);
+
+
+  const taskEdit=(tsk, id)=>{
+    setVal(tsk)
+    setBtnstatus(false);
+    setTaskId(id);
+  }
+
+  const myUpdate=()=>{
+    dispatch(taskUpdate({id:taskId, work:val}));
+     setBtnstatus(true);
+     setVal("");
+  }
+
+
+
   const ans= myTask.map((key, index)=>{
      return(
       <>
          <tr>
           <td>{index+1}</td>
-          <td>{key.work}</td>
+          <td>
+            
+            {key.status=="complete"?(<>
+              <span style={{color:"red",textDecoration: "line-through" }}>{key.work}</span>
+            </>):(<>
+                    {key.work}
+            </>)}
+          
+            
+            
+            </td>
           <td>
             <MdDeleteSweep onClick={()=>{dispatch(delTask({id:key.id}))}} />
           </td>
           <td>
-            <FaCircleCheck />
+            <FaCircleCheck onClick={()=>{dispatch(taskComplete({id:key.id}))}} />
           </td>
-          <td>
+          <td onClick={()=>{dispatch(taskIncomplete({id:key.id}))}}>
             <ImCross />
           </td>
           <td>
-            <button>Edit</button>
+            <button onClick={()=>{taskEdit(key.work, key.id)}}>Edit</button>
           </td>
          </tr>
       </>
@@ -36,7 +64,19 @@ const App=()=>{
       <h1> To Do App </h1>
       <hr />
       Enter Task : <input type="text" value={val} onChange={(e)=>{setVal(e.target.value)}} />
-      <button onClick={()=>{dispatch(addTask({id: Date.now(), work:val}))}}>Add</button>
+    {btnStatus?(<>
+    
+     <button onClick={()=>{dispatch(addTask({id: Date.now(), work:val, status:"incomplete"}))}}>Add</button>
+    </>):(<>
+     
+      <button onClick={myUpdate}>update</button>
+    </>)}
+    
+     
+    
+     
+    
+    
       <hr />
       <table>
         <tr>
--- client/src/todoSlice.jsx
+++ client/src/todoSlice.jsx
@@ -13,9 +13,36 @@ const todoSlice = createSlice({
         delTask:(state, actions)=>{
             state.task= state.task.filter(item=>item.id!=actions.payload.id)
            
+        },
+        taskComplete:(state, actions)=>{
+            for(var i=0; i<state.task.length; i++){
+                 if (state.task[i].id==actions.payload.id)
+                 {
+                    state.task[i].status="complete";
+                 }
+            }
+        },
+        taskIncomplete:(state, actions)=>{
+             for(var i=0; i<state.task.length; i++){
+                 if (state.task[i].id==actions.payload.id)
+                 {
+                    state.task[i].status="incomplete";
+                 }
+            }
+        },
+
+        taskUpdate:(state, actions)=>{
+            for (var i=0; i<state.task.length; i++)
+            {
+                if (state.task[i].id==actions.payload.id)
+                {
+                    state.task[i].work=actions.payload.work;
+                }
+            }
+           
         }
      }
 })
 
-export const {addTask, delTask}  = todoSlice.actions;
+export const {addTask, delTask, taskComplete, taskIncomplete, taskUpdate}  = todoSlice.actions;
 export default todoSlice.reducer;
